854557204709f196b267a35d58df9e37113f8d64
Alex Wong <alexwongweilun@hotmail.co.uk>
2016-11-08 17:42:18 +0800

    added validations for ingredient and recipe models, cannot undo changes to user schema in db schema

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,8 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
+  before_save { name.downcase! }
+  validates :name, presence: true, length: { minimum: 2, maximum: 255 }, uniqueness: { case_sensitive: false }
+  validates :category, presence: true
+  validates :cost, presence: true
+  validates :qtyunit, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108034757) do
+ActiveRecord::Schema.define(version: 20161107142059) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
