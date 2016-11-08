ca3d7a39a1b89debbccad000edc36f6222c1c503
Shi Hao Tan <shihaotan91@gmail.com>
2016-11-08 18:54:49 +0800

    populated ingredients list for paneer

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-onion = Ingredients.create
-onion.name = "Onion"
-onion.category = "Vegetables"
-onion.cost = 0.70
-onion.qtyunit = "NOS"
+onion = Ingredient.create :name => "Onion", :category => "Vegetables", :cost => 0.70, :qtyunit => "NOS"
+
+garlic = Ingredient.create :name => "Garlic", :category => "Vegetables", :cost => 0.20, :qtyunit => "clove"
+
+tumeric_powder = Ingredient.create :name => "Tumeric Powder", :category => "Condiments", :cost => 0.30, :qtyunit => "TBSP"
+
+cream = Ingredient.create :name => "Cream", :category => "Dairy and Eggs", :cost => 0.30, :qtyunit => "100ml"
+
+paneer = Ingredient.create :name => "Paneer", :category => "Dairy and Eggs", :cost => 1.70, :qtyunit => "100g"
+
+ginger = Ingredient.create :name => "Ginger", :category => "Vegetable", :cost => 0.60, :qtyunit => "100g"
+
+spinach = Ingredient.create :name => "Spinach", :category => "Vegetable", :cost => 0.50, :qtyunit => "100g"
+
+tomato = Ingredient.create :name => "Tomato", :category => "Vegetable", :cost => 0.50, :qtyunit => "100g"
+
+basmati_rice = Ingredient.create :name => "Basmati Rice", :category => "Grains", :cost => 0.80, :qtyunit => "100g"
