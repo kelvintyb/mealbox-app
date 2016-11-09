f3a23756bc2b776ab70b8139724971feca0dd9eb
Alex Wong <alexwongweilun@hotmail.co.uk>
2016-11-09 13:55:21 +0800

    added current user id when creating recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,11 +3,9 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   has_many :transactions
-  validates :user, presence: true
-  validates :name, presence: true
-  validates :cuisine, presence: true
-  validates :costperserving, presence: true
-  validates :views, presence: true
-  validates :instructions, presence: true
-  validates :image, presence: true
+  # validates :name, presence: true
+  # validates :cuisine, presence: true
+  # validates :costperserving, presence: true
+  # validates :instructions, presence: true
+  # validates :image, presence: true
 end
