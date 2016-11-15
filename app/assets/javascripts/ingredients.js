571a0dcaf1599623baaa06c9e59e4a6440dd0467
Alex Wong <alexwongweilun@hotmail.co.uk>
2016-11-15 16:47:19 +0800

    merged huda changes to ing js

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -1,11 +1,21 @@
 $(document).on('turbolinks:load', function () {
-  $('#searchform').on('ajax:success', function (e, data, status) {
-    $('.searchcontainer').text('')
-    console.log(data.result)
-    data.result.forEach(function (ingredient) {
-      var newList = $('<li>')
-      newList.text(ingredient.fields.item_name)
-      $('.searchcontainer').append(newList)
-    })
-  })
+ $('#searchform').on('ajax:success', function (e, data, status) {
+   $('.searchcontainer').text('')
+   console.log(data.result)
+   var i = 1
+   data.result.forEach(function (movie) {
+     var newList = $('<li>')
+     var newButton = $('<button class="add-ing"><p>Add Ingredient</p>')
+     newList.text(movie.fields.item_name)
+     newList.addClass('nutriApi' + (i))
+     newList.attr('data-id', JSON.stringify(movie.fields))
+     $('.searchcontainer').append(newList)
+     newList.append(newButton)
+     i++
+   })
+   $('.add-ing').on('click', function () {
+     // $(this).data
+     console.log($(this).data)
+   })
+ })
 })
