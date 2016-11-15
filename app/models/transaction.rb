1ae0699b14e67dad86f7dc0cc5c3a0b2a9370fff
Shi Hao Tan <shihaotan91@gmail.com>
2016-11-15 09:56:02 +0800

    ignore this branch

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,4 +7,10 @@ class Transaction < ApplicationRecord
   validates :totalserving, :numericality => { :less_than_or_equal_to => 20, message: "The maximum serving is 20" }
   validates :totalserving,  presence: { message: "Please choose how many servings you want." }
 
+  validates :creditcard,  length: {minimum: 16, maximum: 16, message: "Please enter a 16 digit credit card number(without spaces)." }
+  validates :creditcard,  presence: { message: "Please enter your credit card number" }
+
+  validates :address1, presence: { message: "Please enter your address"}
+  validates :address2,  presence: { message: "Please enter your apartment/unit"}
+
 end
