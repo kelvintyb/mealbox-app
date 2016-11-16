09afbe4e304a55d7002f93fe950f443648a85cef
Kelvin Tan <kelvin.tyb@gmail.com>
2016-11-17 01:16:10 +0800

    got ajax search working but masonry not loading properly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,11 +23,21 @@
 $(document).on('turbolinks:load', function () {
   $('.hero_search').on('ajax:success', function (e, data, status) {
     $('.hero_remove').empty()
-    $('.hero_result').text('')
+    if (data == []){
+      var QueryNotFound = `<h1>No recipes found. Please try again!</h1><span class="headerLine"></span><span class="add-recipe-btn"><i class="fa fa-plus" aria-hidden="true"></i><a href="/recipes/new">Add Recipe</a>
+      </span><div style="clear:both;"></div>`
+      $(".hero_remove").append(QueryNotFound)
+    }
+    else {
+      var masonryStarter =  `<h1>Search Results</h1><span class="headerLine"></span><span class="add-recipe-btn"><i class="fa fa-plus" aria-hidden="true"></i><a href="/recipes/new">Add Recipe</a>
+      </span><div class="recipe-all"><div class="masonry-container"><div class="grid-sizer"></div></div>
+      </div><div style="clear:both;"></div>`
+      $('.hero_remove').append(masonryStarter)
+      data.search_recipes.forEach(function(recipe){
+        var masonryTile = `<div class="thumbnail recipe-thumb grid-item"><figure class="snip1268"><div class="image"><img src="${recipe.image}" alt="sq-sample4" class="hoverZoomLink"><div class="icons"><div class="recipe-views"><span class="glyphicon glyphicon-eye-open views-icon"></span>${recipe.views}</div></div><a class="view-recipe" href="/recipes/${recipe.id}">View Recipe</a></div></figure><div id="ribbon"><span id="content-cuisine">${recipe.cuisine}</span></div><div class="caption"><h3>${recipe.name}</h3></div></div>`
+        $(".masonry-container").masonry().append(masonryTile).masonry('appended', masonryTile).masonry();
+      })
+    }
     console.log(data.search_recipes)
-    data.search_recipes.forEach(function (recipe) {
-      var newList = $(`<div class="thumbnail recipe-thumb grid-item"><figure class="snip1268"><div class="image"><img src="${recipe['image']}" alt="sq-sample4"><div class="icons"><div class="recipe-views"><span class="glyphicon glyphicon-eye-open views-icon"></span> ${recipe.views} </div></div> link_to 'View Recipe', recipe_path(recipe), class:'view-recipe' </div></figure><div id="ribbon"><span id="content-cuisine"> ${recipe.cuisine} </span></div><div class="caption"><h3> ${recipe.name}</h3></div></div>`)
-      $('.hero_result').append(newList)
-    })
   })
 })
