ff7fa44448bf81bdbf5c1a5ffe4454dfd9b9ee1e
Alex Wong <alexwongweilun@hotmail.co.uk>
2016-11-16 16:11:51 +0800

    api done

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,38 +1,37 @@
 class IngredientsController < ApplicationController
 
+  # appid and appkey codes for external api nutritionix (100 calls per day)
+  # nutri ingredient class variable, used for def update
   @@appid = "b4b15aeb"
   @@appkey = "cd0ec70b471170cb6dd54055513ef1d5"
   @@nutri_ingredient = ''
 
   # admin view all ingredients in db
   def index
+    admincheck()
     @ingredients = Ingredient.all
   end
 
   # admin to search for ingredients to add
   def new
-
+    admincheck()
   end
 
   # admin wants to add the ingredients
+  # using the id from searchingredient post, do a get api call
+  # save the data in the class variable for use later
   def show
+    admincheck()
     @@nutri_ingredient = HTTParty.get("https://api.nutritionix.com/v1_1/item?id=#{params[:id]}&appId=#{@@appid}&appKey=#{@@appkey}")
     @nutri = @@nutri_ingredient
   end
 
-
-  def edit
-
-  end
-
-  def create
-
-    redirect_to transactions_path
-  end
-
+  # if ingredient exists in db, render the show page
+  # else save the ingredient in the db
   def update
-    @found = Ingredient.find_by(name: params['name'])
-    if @found
+    admincheck()
+    found = Ingredient.find_by(name: params['name'])
+    if found
       render 'show'
     else
       nutri_ingredient = Ingredient.new()
@@ -51,13 +50,19 @@ class IngredientsController < ApplicationController
     end
   end
 
+  # find the ingredient in the db and delete it
   def destroy
+    admincheck()
     @ingredient = Ingredient.find(params[:id])
     @ingredient.destroy
     redirect_to ingredients_path
   end
 
+  # if user search query exists in db, tell him it exists
+  # search for the query using the nutritionix get api calls
+  # return a json which can be retrieved in ing js using data.result or data.found
   def searchnutri
+    admincheck()
     keyword = params[:query]
     ing_exist = Ingredient.find_by(name: keyword)
     if ing_exist
@@ -72,8 +77,17 @@ class IngredientsController < ApplicationController
     }
   end
 
+  # redirect to show with the id provided
   def searchingredient
+    admincheck()
     redirect_to ingredient_path(params[:nutri_id])
   end
 
+  private
+    # if user is not login or not admin, redirect to root
+    def admincheck
+      if !current_user || current_user.id != 1
+        redirect_to root_path
+      end
+    end
 end
