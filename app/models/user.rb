71992e664d17d6f04fff555852d3f5164a561398
Nurul Huda <huddyhuda@gmail.com>
2016-11-19 20:30:21 +0800

    yeah

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :recipes
+  has_many :recipes, dependent: :destroy
   has_many :transactions
   # validates :name, presence: { message: "Please provide a name." }, length: {"Invalid name found"}
   # validates :contactno, uniqueness: true, length: { minimum: 8}
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   #
   # validates :address1, presence: { message: "Please enter your address"}
   # validates :address2,  presence: { message: "Please enter your apartment/unit"}
-  
+
   validates :name, presence: { message: "Please enter your name"}
   validates :contactno, presence: { message: "Please enter your contact number"}
   validates :birthdate, presence: { message: "Please enter your birthdate"}
